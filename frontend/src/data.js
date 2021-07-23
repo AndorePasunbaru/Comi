e0e8270ced2f4335dd22f099e64ef168bebc9630
Andre <zechkaiser@gmail.com>
2021-07-23 11:52:20 +0800

    first commit

---
+++ frontend/src/data.js
@@ -0,0 +1,17 @@
+const data = {
+  products: [
+    {
+      _id: "1",
+      name: "Nike Slim Shirt",
+      category: "Shirts",
+      image: "/images/1.jpg",
+      price: 120,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: "High Quality product",
+    },
+  ],
+};
+
+export default data;
